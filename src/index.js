2ea3fbc6d54d4853fe7fbab81a1660e4f25ba221
Uglekiska <uglekiska@yandex.by>
2021-08-25 17:53:44 +0300

    fix: update index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+        let array = [];
+        let bracketsArray = bracketsConfig.flat();
+        const newStr = str.split('');
+        for (let i = 0; i < newStr.length; i++) {
+            if ((newStr[i] === bracketsArray[1] && array[array.length - 1] === bracketsArray[0]) || (newStr[i] === bracketsArray[3] && array[array.length - 1] === bracketsArray[2]) || (newStr[i] === bracketsArray[5] && array[array.length - 1] === bracketsArray[4]) || (newStr[i] === bracketsArray[7] && array[array.length - 1] === bracketsArray[6]) || (newStr[i] === bracketsArray[9] && array[array.length - 1] === bracketsArray[8])) {
+                array.pop(newStr[i - 1]);
+            } else {
+                array.push(newStr[i]);}}
+    return array.length === 0}
